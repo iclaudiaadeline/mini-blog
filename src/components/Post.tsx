d478b4a21d73a6da9d3aac11558cc46713d2dadc
iclaudiaadeline <a.iradukund3@alustudent.com>
2026-09-21 20:31:06 +0200

    feat: add "New!" badge to Post component for recently added posts

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -5,9 +5,10 @@ interface PostProps {
   post: PostType;
 }
 function Post({ post }: PostProps) {
+    const isNew = Date.now() - new Date(post.date).getTime() < 24 * 60 * 60 * 1000;
     return (
       <article className="post-card">
-        <h2>{post.title}</h2>
+        <h2>{post.title}{isNew && <span className="new-badge">New!</span>}</h2>
         <p>{post.author}</p>
         <p>{post.content}</p>
         <p>{post.date}</p>
