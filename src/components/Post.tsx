b90c8e1b90d5a4a17acf1ac08b7c97b490810667
iclaudiaadeline <a.iradukund3@alustudent.com>
2026-09-21 21:16:44 +0200

    feat: optimize Post component by memoizing for performance

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import '../styles/posts.css';
 import type { Post as PostType } from '../types/post';
 
@@ -16,4 +17,4 @@ function Post({ post }: PostProps) {
     );
 }
 
-export default Post;
+export default memo(Post);
