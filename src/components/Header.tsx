1ddf2d8014ed31c2600f969180302565667f6faf
iclaudiaadeline <a.iradukund3@alustudent.com>
2026-09-21 16:38:20 +0200

    feat: add Header component for site navigation and title

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header>
+      <h1>{'Dev Insights'}</h1>
+      <nav>
+        <a href="#">{'New Post'}</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
